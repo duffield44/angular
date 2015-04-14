522a4a6237d0dedb84117ae04e111db1453b9c6c
duffield44 <duffieldbenjamin44@gmail.com>
2015-04-14 20:54:34 +0800

    Add images and iterate through products to print name, price & image

--- js/app.js
+++ js/app.js
@@ -2,13 +2,22 @@
 var app = angular.module('store', []);
 
 app.controller('StoreController', function(){
-	this.product = gem;
+	this.products = gems;
 });
 
-var gem = {
-	name: 'Dodecahedron',
-	price: 2.95,
-	description: '.....'
-}
+var gems = [
+	{
+		name: 'Dodecahedron',
+		price: 2.95,
+		description: 'Some gems have hidden qualities beyond their luster, beyond their shine... Dodeca is one of those gems.',
+		image: "images/gem-01.gif"
+	},
+	{
+		name: 'Pentagonal Gem',
+		price: 5.95,
+		description: 'Origin of the Pentagonal Gem is unknown, hence its low value. It has a very high shine and 12 sides however.',
+		image: "images/gem-02.gif"
+	}
+];	
 
 })();
