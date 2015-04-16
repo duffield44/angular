43656b14f36b9596acee6aab422baf8535f5731c
duffield44 <duffieldbenjamin44@gmail.com>
2015-04-16 14:04:36 +0800

    Change gem items, add thumbnails using GalleryController. Add comments to index.html

--- js/app.js
+++ js/app.js
@@ -5,23 +5,6 @@ app.controller('StoreController', function(){
 	this.products = gems;
 });
 
-var gems = [
-	{
-		name: 'Dodecahedron',
-		price: 2.95,
-		description: 'Some gems have hidden qualities beyond their luster, beyond their shine... Dodeca is one of those gems.',
-		image: "images/gem-01.gif",
-		reviews: []
-	},
-	{
-		name: 'Pentagonal Gem',
-		price: 5.95,
-		description: 'Origin of the Pentagonal Gem is unknown, hence its low value. It has a very high shine and 12 sides however.',
-		image: "images/gem-02.gif",
-		reviews: []
-	}
-];	
-
 app.controller('PanelController', function(){
 	this.tab = 1;
 	this.selectTab = function(setTab){
@@ -40,5 +23,48 @@ app.controller('ReviewController', function(){
 		this.review = {};
 	};
 });
+
+app.controller('GalleryController', function(){
+	this.current = 0;
+	this.setCurrent = function(imageNumber){
+      this.current = imageNumber || 0;
+    };
+});
+
+var gems = [
+	{
+		name: 'Azurite',
+      	description: "Some gems have hidden qualities beyond their luster, beyond their shine... Azurite is one of those gems.",
+      	price: 110.50, 
+		images: [
+	        "images/gem-02.gif",
+	        "images/gem-05.gif",
+	        "images/gem-09.gif"
+      	],
+		reviews: []
+	},
+	{
+		name: 'Bloodstone',
+      	description: "Origin of the Bloodstone is unknown, hence its low value. It has a very high shine and 12 sides, however.",
+      	price: 22.90,
+		images: [
+	        "images/gem-01.gif",
+	        "images/gem-03.gif",
+	        "images/gem-04.gif"
+      	],
+		reviews: []
+	},
+	{
+		name: 'Zircon',
+        description: "Zircon is our most coveted and sought after gem. You will pay much to be the proud owner of this gorgeous and high shine gem.",
+        price: 1100,
+        rarity: 2,
+        images: [
+          "images/gem-06.gif",
+          "images/gem-07.gif",
+          "images/gem-08.gif"
+        ],
+	}
+];	
 
 })();
